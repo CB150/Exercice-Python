0c71c36dba7d436783b7f111b4987496494ec1df
Skorvo <clement.berhondo@edu.itescia.fr>
2019-10-30 19:39:54 +0100

    Exercice 17.py

--- Exercice 17.py
+++ Exercice 17.py
@@ -1,16 +1,18 @@
+#création de la fonction avec 1 paramètre
 def palindrome(str):
 	print("message", str)
+	#si la taille du 1er paramètre tombe a 1 ou à 0, alors on renvoie True car le mot est palindrome
 	if str == [] or len(str) == 1:
 		return True
 	else:
-		if str[0] == str[len(str) - 1]:
-
+		#sinon, si le premier caractère du paramètre est égale au dernier caractère,
+		if str[0] == str[len(str) - 1]: alors on enlève le premier et le dernier caractère de la liste et on relance la fonction
 			str = str[1:]
 			str = str[:-1]
 			return (palindrome(str))
 		else:
+			#si la condition d'avant est fausse, alors on renvoie False
 			return False
 
-ant=input("entre le mot ")
-print(palindrome(ant))
-#print(palindrome("kayak"))
+mot=input("entre le mot ")
+print(palindrome(mot))
--- Exercice 18.py
+++ Exercice 18.py
@@ -1,25 +1,30 @@
+#création de la fonction avec 3 paramètres
 def trouverLeCoupableIte(suspect, brin1, brin2):
 	brin1in = False
 	brin2in = False
-
 	i = 0
 	tmp = []
+
+	#tant que brin1in ou brin2in sont faux et i est inférieur au nombre d'éléments de suspect moins le nombre d'éléments de brin1
 	while ((not brin1in or not brin2in) and i < len(suspect) - len(brin1)):
 
-		tmp = suspect[i:]
-		tmp = tmp[:-(len(suspect) - len(brin1) - i)]
+		tmp = suspect[i:]#supprime les i éléments de la liste
+		tmp = tmp[:-(len(suspect) - len(brin1) - i)] #laisse 4 éléments dans la liste
 
+        #si tmp = brin 1 et que brin1in est faux
 		if (brin1 == tmp and brin1in == False):
-			i += 4
-			brin1in = True
+			i += 4 #on incrémente i de 4
+			brin1in = True #brin1in devient vrai
+        #si tmp = brin 2 et que brin2in est faux
 		elif (brin2 == tmp and brin2in == False):
-			i += 4
-			brin2in = True
+			i += 4 #on incrémente i de 4
+			brin2in = True #brin1in devient vrai
+		#si brin1in = brin2in = True
 		elif (brin1in == brin2in == True):
-			return (brin1in and brin2in)
+			return (brin1in and brin2in) #on renvoie leur valeur
+		#sinon on incrémente i de 1
 		else:
 			i += 1
-
 	return (brin1in and brin2in)
 
 rose="CTCCTGATGCTCCTCGCTTGGTGGTTTGAGTGGACCTCCCAGGCCAGTGCCGGGCCCCTCATAGGAGAGGAD"
