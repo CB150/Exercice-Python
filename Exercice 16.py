0a43a740cb29805f43bffeba5075570681f17791
Skorvo <clement.berhondo@edu.itescia.fr>
2019-10-30 19:52:45 +0100

    Ajoutez Exercice 16

--- Exercice 16.py
+++ Exercice 16.py
@@ -1,12 +1,11 @@
 #création de la fonction avec 2 paramètres
 def distance_hamming(str1,str2):
-	#mise à 0 du compteur
+
 	cpt=0
 	#si la taille d premier mot n'est pas égale à celle du deuxième
 	if (len(str1)!=len(str2)):
 		#le programme renvoie la phrase ci-dessous et met fin au programme.
 		return("Les mots ne sont pas de la même longueur")
-	#sinon pour i = a la taille du premier paramètre
 	else:
 		for i in range(len(str1)):
 			#si l'itérateur de la boucle du premier paramètre est différent de l'itérateur du second paramètre
